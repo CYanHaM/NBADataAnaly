78b22d7f0b1cd82e99711c98b4ee2fee47c7295a
turtlology <504350677@qq.com>
2015-03-20 20:30:19 +0800

    BLfinish

--- src/blservice/TeamTechBLservice.java
+++ src/blservice/TeamTechBLservice.java
@@ -6,27 +6,16 @@ import TypeEnum.TeamTechEnum;
 import VO.TeamTechVO;
 
 public interface TeamTechBLservice {
-    public ArrayList<TeamTechVO> AscendAll(TeamTechEnum DataType);
+    public ArrayList<TeamTechVO> Ascend(TeamTechEnum DataType);
 	/* 传入排序的类型
 	 * 按赛季总数据排序
 	 * 升序
      * */
     
-    public ArrayList<TeamTechVO> DescendAll(TeamTechEnum DataType);
+    public ArrayList<TeamTechVO> Descend(TeamTechEnum DataType);
 	/* 传入排序的类型
 	 * 按赛季总数据排序
 	 * 降序
      * */
     
-    public ArrayList<TeamTechVO> AscendAve(TeamTechEnum DataType);
-	/* 传入排序的类型
-	 * 按场均数据排序
-	 * 升序
-     * */
-    
-    public ArrayList<TeamTechVO> DescendAve(TeamTechEnum DataType);
-	/* 传入排序的类型
-	 * 按场均数据排序
-	 * 降序
-     * */
 }
